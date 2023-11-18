d24dad784db2bab0fd3d0efc012148443252ee0b
w-garden <w.garden316@gmail.com>
2023-11-18 20:55:55 +0900

    ch03 프로젝트 생성

---
+++ VOL1/ch03/src/springbook/user/UserDaoTest.java
@@ -0,0 +1,37 @@
+package springbook.user;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+
+public class UserDaoTest {
+    @Test
+    public static void main(String[] args) throws SQLException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        int deleteRows=dao.deleteAll();
+        System.out.println(deleteRows+" 개 데이터 삭제성공!!!");
+
+        User user = new User();
+        user.setId("shc729");
+        user.setName("신호철");
+        user.setPassword("123456");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println("user2.getName() : " + user2.getName());
+        System.out.println("user2.getPassword) : " + user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회성공");
+    }
+
+}
