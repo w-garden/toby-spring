275deedb4fc6f4721915c5634e4e1b64be73c25d
w-garden <w.garden316@gmail.com>
2023-12-06 23:56:44 +0900

    UserDao 인터페이스 추가

---
+++ VOL1/ch04/src/UserDao.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public interface UserDao {
+    void add(User user);
+
+    User get(String id);
+    List<User> getAll();
+    void deleteAll();
+    int  getCount();
+
+}
