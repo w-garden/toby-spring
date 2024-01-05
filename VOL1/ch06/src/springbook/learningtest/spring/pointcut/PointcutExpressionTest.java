51cb777c0824a8d4c05d247e8e9f0d6e9ea6629b
w-garden <w.garden316@gmail.com>
2024-01-05 23:52:37 +0900

    테스트 헬퍼 메서드 작성_494p

--- VOL1/ch06/src/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
+++ VOL1/ch06/src/springbook/learningtest/spring/pointcut/PointcutExpressionTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 
+import javax.mail.search.SearchTerm;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -18,6 +20,10 @@ public class PointcutExpressionTest {
                 Target.class.getMethod("minus", int.class, int.class), null), is(true));
         assertThat(pointcut.getClassFilter().matches(Target.class) && pointcut.getMethodMatcher().matches(
                 Target.class.getMethod("plus", int.class, int.class), null), is(false));
+
+    }
+
+    public void pointcutMatches(String expression, Boolean expected, Class<?> clazz, String methodName, Class<?>... args) throws Exception {
 
     }
 }
