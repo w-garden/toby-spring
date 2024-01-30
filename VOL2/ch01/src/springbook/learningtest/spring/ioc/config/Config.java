831f18289280846e5514953d0eda041b595fbc7b
w-garden <w.garden316@gmail.com>
2024-01-30 23:00:24 +0900

    Config.java 클래스 추가

---
+++ VOL2/ch01/src/springbook/learningtest/spring/ioc/config/Config.java
@@ -0,0 +1,18 @@
+package springbook.learningtest.spring.ioc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springbook.learningtest.spring.ioc.bean.Hello;
+import springbook.learningtest.spring.ioc.bean.Printer;
+
+@Configuration
+public class Config {
+    @Bean
+    public Hello hello(){
+        return new Hello();
+    }
+    @Bean
+    public Printer printer(){
+        return new Printer();
+    }
+}
